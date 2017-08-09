4d26d30a2a0ab8085949aa7bd9cb506cdb220ad8
Cyril Siman <csiman.phone@gmail.com>
2017-08-10 00:31:59 +0200

    Use Tag parameter as Tag for login button

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -67,9 +67,9 @@ class GoogleLogin extends Component {
 
     const Tag = tag
 
-    return <button onClick={this.signIn} disabled={disabled} className="react-google-oauth-button-login">
+    return <Tag onClick={this.signIn} disabled={disabled} className="react-google-oauth-button-login">
             {children ? children : buttonText}
-           </button>
+           </Tag>
 
   }
 }
